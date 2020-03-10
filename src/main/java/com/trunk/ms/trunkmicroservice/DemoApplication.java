f059c073a2b93e345bf7316a77891cc6af520447
shahinkl <shahin@saal.ai>
2020-03-10 17:00:02 +0400

    Initial import

---
+++ src/main/java/com/trunk/ms/trunkmicroservice/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.trunk.ms.trunkmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trunk/ms/trunkmicroservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trunk.ms.trunkmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
